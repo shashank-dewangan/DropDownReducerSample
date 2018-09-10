28e572525205b9b63f05c420a721f9d6e73b9669
shashank-dewangan <shashank.at30@gmail.com>
2018-09-10 15:39:02 +0530

    refactoring

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import GroupDropDown from "./GroupDropDown";
-import CustomDropdown from "./CustomDropDown.jsx";
 import { reducer } from "./reducer";
 import { createStore } from "redux";
 import "./styles.css";
-import Dropdown from "react-dropdown";
 import "react-dropdown/style.css";
 
 const App = ({ optionGroup, OnAddItem, clearCollection }) => {
